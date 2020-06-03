14d6c7005e1fded1a4b9a0f5d543c4542a20d652
maxalex <gasolinchen@online.de>
2020-06-03 12:24:21 +0200

    change in website html header

--- website/html_header.php
+++ website/html_header.php
@@ -1,4 +1,7 @@
-   	<meta charset="utf-8" />
+    <!-- This file is designed to be included in every page of the website -->
+
+    <!-- It adds metadata and other globally important data -->
+    <meta charset="utf-8" />
     <meta name="description" content="This is an unofficial fan-based website for the MMORPG PlaneShift." />
     <meta name="author" content="Huarwar Eruera" />
     <meta name="keywords" content="planeshift, plane shift, mmorpg, trade, exchange, hydlaa, yliakum, klyros, maps, npc, npcs, quests, quest, magic" />
@@ -7,4 +10,5 @@
     <link href="/images/favicon.ico?r=31241" type="image/x-icon" rel="shortcut icon"/>
     <link href="/images/apple-touch-icon.png" rel="apple-touch-icon" />
 
+    <!-- The base style sheet which is included in every single page -->
     <link href="/styles/base.css" type="text/css" rel="stylesheet" />
