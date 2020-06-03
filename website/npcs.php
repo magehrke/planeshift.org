135738df237678d630cbf8bb8cb7b41dff8c36f7
maxalex <gasolinchen@online.de>
2020-06-03 20:55:59 +0200

    cosmetic

--- website/npcs.php
+++ website/npcs.php
@@ -2,16 +2,16 @@
 <html>
 <head>
 	<title>NPC&apos;s</title>
-    
+
 	<?php
         $path = $_SERVER['DOCUMENT_ROOT'];
         $path .= "/html_header.php";
         include($path);
     ?>
-    
+
 	<link href="/styles/npcs.css" type="text/css" rel="stylesheet" />
 
-    
+
 </head>
 
 <!-- BODY -->
@@ -21,41 +21,40 @@
 	<tr>
     	<td>
         	<div id="root_site_container">
-            
+
             	<!-- include Header and Navigation -->
 				<?php
 					$path = $_SERVER['DOCUMENT_ROOT'];
 					$path .= "/header_and_navigation.php";
                     include($path);
                 ?>
-                
+
                 <!-- Content section -->
                 <div id="content_main_container">
-                
+
                 	<table class="npcs_main_table">
-                    
+
                         <!-- NPCs Tabelle füllen -->
                         <?php
 
                             /*
                              * Get the total number of npc lines in our fill database file.
-                             * 1 Line = 1 npc count here = all npcs with the same name
+                             * 1 Line = 1 npc count = all npcs with the same name
                              * at one location (e.g. all Gladiators in Arena = 1 NPC).
                              */
-                            $abfrage = "SELECT name
-                            			FROM npcs";
-                            $get_num_of_npcs = $mysqli->query($abfrage) or die(mysql_error());
+                            $query = "SELECT name FROM npcs";
+                            $get_num_of_npcs = $mysqli->query($query) or die(mysql_error());
                             $num_of_npcs = $get_num_of_npcs->num_rows;
 
                             /* Set title with npc count */
                             echo "<h1>NPC's (#" . $num_of_npcs . ")</h1>";
-                                
+
 
                             /* This is the request we will use for the table */
-                            $abfrage = "SELECT DISTINCT name, left(name, 1) AS Letter 
+                            $abfrage = "SELECT DISTINCT name, left(name, 1) AS Letter
                             			FROM npcs
                             			ORDER BY Letter ASC";
-                            $get_npc_name = $mysqli->query($abfrage) or die(mysql_error());	
+                            $get_npc_name = $mysqli->query($abfrage) or die(mysql_error());
 
                             /*
                              * We want to split the T section. So we need to know when the
@@ -64,16 +63,16 @@
                              */
                             $request_T = "SELECT DISTINCT name
                             			FROM npcs
-                            			WHERE name 
+                            			WHERE name
                             			LIKE 'T%'";
-                            $get_amount_of_T = $mysqli->query($request_T) or die(mysql_error());	
+                            $get_amount_of_T = $mysqli->query($request_T) or die(mysql_error());
                             $half_amount_of_T = round(($get_amount_of_T->num_rows) / 2 + 2);
-                            
+
                             /* In this variable do we save the starting Letter of the last
                              * NPC, so we can check, if we have a new starting Letter and
                              * if so, we can make a new section */
                             $oldLetter = "A";
-							
+
 							// We count the TD, cause we want to add a new row every
                            	// X TD's.
 							$counterTDsections = 1;
@@ -95,7 +94,7 @@
                             	// Do we start a new column with new first letter names?
                             	// Also we dont want a new column at Z, cause we put Y/Z together
                             	// Also we dont want a new column at Q, cause we put P/Q together
-                            	if($npc_name["Letter"] != $oldLetter 
+                            	if($npc_name["Letter"] != $oldLetter
                             		&& $npc_name["Letter"] != "Z"
                             		&& $npc_name["Letter"] != "Q") {
                             		$oldLetter = $npc_name["Letter"];
@@ -170,9 +169,9 @@
 								if($get_npc_location->num_rows == 1) {
 
 									echo "<a href='";
-									
+
 									/* Printing the NPC with a link to the Map */
-									/* The Variable $npcName has to be set !!! */	
+									/* The Variable $npcName has to be set !!! */
 									$npcName = $npc_name["name"];
 							        $path = $_SERVER['DOCUMENT_ROOT'];
 							        $path .= "/includes/npc_link_to_map.inc.php";
@@ -182,56 +181,56 @@
 
 								} else {
 									echo "<span class=\"npc_name\">" . $npc_name["name"] . ":</span>";
-								
+
 
 								echo "<br>";
 
-								
+
 									/* Before the first entry shall not be a break, so we need to have it seperated */
-									echo "<a href=\"/maps/" 
+									echo "<a href=\"/maps/"
 										. strtolower(str_replace(' ', '-', $npc_location["area"]))
 										. ".php#"
 										. str_replace(' ', '_', $npc_name["name"])
-										. "\">" 
-										. $npc_location["area"] 
-										. " (" 
+										. "\">"
+										. $npc_location["area"]
+										. " ("
 										. $npc_location["quantity"] . ")</a>";
-									
+
 									/* Now we can put a break in front */
 									while($npc_location = $get_npc_location->fetch_array()) {
 										echo "<br>";
 
-										echo "<a href=\"/maps/" 
+										echo "<a href=\"/maps/"
 											. strtolower(str_replace(' ', '-', $npc_location["area"]))
 											. ".php#"
 											. str_replace(' ', '_', $npc_name["name"])
-											. "\">" 
-											. $npc_location["area"] 
-											. " (" 
+											. "\">"
+											. $npc_location["area"]
+											. " ("
 											. $npc_location["quantity"] . ")</a>";
 
 									}
 								}
 								echo "</li>";
-									
+
                             } // End while loop.
                             echo "</nav>";
                             echo "</td>";
                             echo "</tr>";
-                        
+
                         ?>
-                    
+
                     </table>
-                    
+
                 </div>
-                
+
                 <!-- include Footer -->
 				<?php
 					$path = $_SERVER['DOCUMENT_ROOT'];
 					$path .= "/footer.php";
                     include($path);
                 ?>
-            
+
             </div>
         </td>
     </tr>
@@ -239,5 +238,3 @@
 
 </body>
 </html>
-
-
