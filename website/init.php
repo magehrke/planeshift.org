5304a471479d3f8442afd745c3fa2af69295ef6d
Oliver Schröder <progs@o-schroeder.de>
2020-07-18 20:37:14 +0200

    added jquery

---
+++ website/init.php
@@ -0,0 +1,33 @@
+<!-- Drop and Create all the database tables -->
+<!-- Can be removed if all Data is correct -->
+<?php
+    $path = $_SERVER['DOCUMENT_ROOT'];
+    $path .= "/fill_database/fill_database_jobs.php";
+    include($path);
+
+    $path = $_SERVER['DOCUMENT_ROOT'];
+    $path .= "/fill_database/fill_database_eclipse.php";
+    include($path);
+
+    $path = $_SERVER['DOCUMENT_ROOT'];
+    $path .= "/fill_database/fill_database_items.php";
+    include($path);
+
+    $path = $_SERVER['DOCUMENT_ROOT'];
+    $path .= "/fill_database/fill_database_npcs-maps.php";
+    include($path);
+
+    $path = $_SERVER['DOCUMENT_ROOT'];
+    $path .= "/fill_database/fill_database_questchains.php";
+    include($path);
+
+    $path = $_SERVER['DOCUMENT_ROOT'];
+    $path .= "/fill_database/fill_database_glyphs-spells-magic.php";
+    include($path);
+
+    $path = $_SERVER['DOCUMENT_ROOT'];
+    $path .= "/fill_database/fill_database_recipes.php";
+    include($path);
+    include ($_SERVER['DOCUMENT_ROOT']."/index.php");
+?>
+
