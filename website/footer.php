17c0b94779b04d147041a6fa6737595353e817ca
maxalex <gasolinchen@online.de>
2020-10-19 11:18:25 +0200

    new e-mail address

--- website/footer.php
+++ website/footer.php
@@ -4,38 +4,38 @@
         <table>
             <tr>
                 <td>
-                    <p> 
-                    	&copy; Planeshift.org 2020 
-                    	| e-mail: <a href="mailto:planeshift@online.de">planeshift@online.de</a>
+                    <p>
+                    	&copy; Planeshift.org 2020
+                    	| e-mail: <a href="mailto:planeshift.org@gmail.com">planeshift.org@gmail.com</a>
                     	| <a href="/legalnotice.php">Legal Notice</a>
-                        | Visits:                                 
-                                    <!-- Total Visits: 
-                                    <php 
+                        | Visits:
+                                    <!-- Total Visits:
+                                    <php
                                         # This will print the counter 1
                                         # This counter counts all requests this page is getting
                                         # If a page is reloaded, it is a new request, if you go to a subpage its also a new request
 
-                                        $count = $mysqli->query("SELECT counter FROM counter") 
+                                        $count = $mysqli->query("SELECT counter FROM counter")
                                             or die(mysql_error());
                                         $count = $count->fetch_array();
-                                        echo $count['counter']; 
+                                        echo $count['counter'];
                                     ?>
-                                    
+
                                     -->
 
                                     <?php
                                         # This counter shows all unique ip addresses who connected to kov
-                                        
+
                                         # DISTINCT ist eigentlich nicht nötig, da eine ip adresse ja nur
                                         # eingetragen wird, wenn sie für die jeweilige page (aber wir haben
                                         # nur eine page=kov_mainpage) noch nicht eingetragen wurde.
                                         # Dass heißt, wenn alles richtig ist, sollte es jede userip - page
                                         # kombination nur einmal geben.
-                                        
-                                        $pageview = $mysqli->query("SELECT userip FROM pageview 
+
+                                        $pageview = $mysqli->query("SELECT userip FROM pageview
                                             WHERE page='kov_mainpage' ") or die(mysql_error());
                                         $pageview_count = $pageview->num_rows;
-                                        echo $pageview_count; 
+                                        echo $pageview_count;
                                     ?>
                     </p>
                 </td>
